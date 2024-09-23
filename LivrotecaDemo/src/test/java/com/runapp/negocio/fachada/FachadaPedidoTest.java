286e349bd152d12d917e36ef09a8b22f662d9014
Matheus <jmatheusnl@gmail.com>
2024-09-22 21:50:57 -0300

    Teste de finalizar pedido concluído

--- LivrotecaDemo/src/test/java/com/runapp/negocio/fachada/FachadaPedidoTest.java
+++ LivrotecaDemo/src/test/java/com/runapp/negocio/fachada/FachadaPedidoTest.java
@@ -2,8 +2,9 @@ package com.runapp.negocio.fachada;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -16,17 +17,19 @@ import com.runapp.negocio.cadastro.exception.QuantidadeInsuficienteException;
 import com.runapp.negocio.cadastro.exception.UsuarioDuplicadoException;
 import com.runapp.negocio.cadastro.exception.UsuarioNaoExisteException;
 import com.runapp.negocio.fachada.exception.ClienteNaoExisteException;
+import com.runapp.negocio.fachada.exception.PedidoVazioException;
 import com.runapp.negocio.fachada.exception.QuantidadeInvalidaException;
 
 import jakarta.transaction.Transactional;
 
 @SpringBootTest
 @Transactional
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class FachadaPedidoTest {
 	@Autowired
 	private Fachada fachada;
 
-	@BeforeEach
+	@BeforeAll
 	public void init() throws LivroJaExisteException, UsuarioDuplicadoException {
 		Cliente c = new Cliente("Matheus", "email@example.com", "123456", new Endereco("logradouro", "numero", "bairro", "cep", "uf"));
 		
@@ -39,12 +42,26 @@ class FachadaPedidoTest {
 		Cliente c = fachada.procurarClienteId((long) 1);
 		Livro l = fachada.procurarLivroId((long) 1);
 		int quantItensPedidoCliente = c.getPedidoPendente().getItens().size();
+		double valorPedido = c.getPedidoPendente().getValorTotal();
 		
 		fachada.adicionarLivroPedidoCliente(l.getId(), 1, false, c.getId());
 		
 		int quantItensPedidoCliente2 = c.getPedidoPendente().getItens().size();
 		
 		assertEquals(quantItensPedidoCliente + 1, quantItensPedidoCliente2);
+		assertEquals(valorPedido + 20, c.getPedidoPendente().getValorTotal());
 	}
-
+	
+	@Test
+	void testeFinalizarPedidoCliente() throws UsuarioNaoExisteException, ClienteNaoExisteException, LivroNaoExisteException, QuantidadeInvalidaException, QuantidadeInsuficienteException, UsuarioDuplicadoException, PedidoVazioException {
+		Cliente c = fachada.procurarClienteId((long) 1);
+		Livro l = fachada.procurarLivroId((long) 1);
+		fachada.adicionarLivroPedidoCliente(l.getId(), 1, false, c.getId());
+		
+		double valor = fachada.finalizarPedidoCliente(c.getId());
+		assertEquals(20, valor);
+		assertEquals(c.getPedidoPendente().getItens().isEmpty(), true);
+		assertEquals(c.getPedidoPendente().getStatus(), "PENDENTE");
+		assertEquals(fachada.exibirHistoricoCliente(c.getId()).size(), 1);
+	}
 }
