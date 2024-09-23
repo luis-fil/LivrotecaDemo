458de188484d4f5059f70bed8b7b2c75a7234af3
Matheus <jmatheusnl@gmail.com>
2024-09-23 02:09:23 -0300

    Criação testes de unidade

---
+++ LivrotecaDemo/src/test/java/com/runapp/negocio/cadastro/CadastroMensagemTest.java
@@ -0,0 +1,16 @@
+package com.runapp.negocio.cadastro;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroMensagemTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ LivrotecaDemo/src/test/java/com/runapp/negocio/cadastro/CadastroUsuarioTest.java
@@ -0,0 +1,33 @@
+package com.runapp.negocio.cadastro;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.runapp.negocio.basica.usuarios.Administrador;
+import com.runapp.negocio.basica.usuarios.Cliente;
+import com.runapp.negocio.basica.usuarios.Endereco;
+import com.runapp.negocio.cadastro.exception.UsuarioDuplicadoException;
+
+@SpringBootTest
+class CadastroUsuarioTest {
+	@Autowired
+	private InterfaceCadastroUsuario cadastroUsuario;
+
+	@Test
+	void testarCadastroEmailDuplicado() {
+		String email = "email@gmail.com";	
+		Administrador a = new Administrador("Matheus", email, "12345678");
+		Cliente c = new Cliente("Nogueira", email, "87654321", new Endereco("logradouro", "numero", "bairro", "cep", "uf"));
+		
+		UsuarioDuplicadoException exception = assertThrows(UsuarioDuplicadoException.class, () -> {
+			cadastroUsuario.cadastrarUsuario(a);
+			cadastroUsuario.cadastrarUsuario(c);
+		});
+		assertEquals(exception.getEmail(), email);
+		assertTrue(exception.getMessage().contains("mesmo email"));
+	}
+
+}
